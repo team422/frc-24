3e696b474821fd6344f2c1a46f6fb48860eff79a
keckothedragon <flappystar05@gmail.com>
2024-01-24 21:15:08 -0500

    RollerIO and RollerIOSim done

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "frc-24";
+  public static final String MAVEN_NAME = "FRC-24";
   public static final String VERSION = "unspecified";
   public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "95dc64d19f7f48a7b446155da211b0ffb1289291";
-  public static final String GIT_DATE = "2024-01-24 08:49:12 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-01-24 09:20:41 EST";
-  public static final long BUILD_UNIX_TIME = 1706106041811L;
+  public static final String GIT_SHA = "9caa8636a096ab2faa5b0162c2197a742e71a61f";
+  public static final String GIT_DATE = "2024-01-24 09:47:46 EST";
+  public static final String GIT_BRANCH = "rollers";
+  public static final String BUILD_DATE = "2024-01-24 20:34:07 EST";
+  public static final long BUILD_UNIX_TIME = 1706146447855L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/intake/rollers/RollerIO.java
+++ src/main/java/frc/robot/subsystems/intake/rollers/RollerIO.java
@@ -1,5 +1,17 @@
 package frc.robot.subsystems.intake.rollers;
 
-public interface RollerIO {
-    
+import org.littletonrobotics.junction.AutoLog;
+
+import frc.lib.advantagekit.LoggedIO;
+
+public interface RollerIO extends LoggedIO<RollerIO.RollerInputs> {
+    @AutoLog
+    public static class RollerInputs {
+        public double curVelocity;
+        public double voltage;
+        public double outputCurrent;
+    }
+
+    public void setVoltage(double voltage);
+
 }
